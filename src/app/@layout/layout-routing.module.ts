6281057eccf30e528bf86138b11de0e9916d91d8
ezzabuzaid <ezzabuzaid@hotmail.com>
2019-06-15 13:21:22 +0300

    add fav icons

--- src/app/@layout/layout-routing.module.ts
+++ src/app/@layout/layout-routing.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from '@layout/layout/layout.component';
 
-
 const FEATURES_PATH = '.././@features/';
 
 const routes: Routes = [
   {
-    path: '', component: LayoutComponent
-    // , children: [
-    //   { path: 'dashboard', loadChildren: `${FEATURES_PATH}dashboard/dashboard.module#DashboardModule`, data: { state: 'module' } },
-    //   { path: 'order', loadChildren: `${FEATURES_PATH}order/order.module#OrderModule`, data: { state: 'module' } },
-    //   { path: 'addresses', loadChildren: `${FEATURES_PATH}addresses/addresses.module#AddressesModule`, data: { state: 'module' } },
-    //   { path: 'support', loadChildren: `${FEATURES_PATH}support/support.module#SupportModule`, data: { state: 'module' } },
-    // ]
+    path: '', component: LayoutComponent, children: [
+      { path: 'dashboard', loadChildren: `${FEATURES_PATH}dashboard/dashboard.module#DashboardModule`, data: { state: 'module' } },
+      { path: 'admins', loadChildren: `${FEATURES_PATH}admins/admins.module#AdminsModule`, data: { state: 'module' } },
+      { path: 'order', loadChildren: `${FEATURES_PATH}order/order.module#OrderModule`, data: { state: 'module' } },
+      { path: 'addresses', loadChildren: `${FEATURES_PATH}addresses/addresses.module#AddressesModule`, data: { state: 'module' } },
+      { path: 'drivers', loadChildren: `${FEATURES_PATH}drivers/drivers.module#DriversModule`, data: { state: 'module' } },
+    ]
   }
 ];
 
