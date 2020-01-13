2f86d61dc327584bf7e750c2576fafb4600ca8c0
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-01-13 19:07:35 +0200

    fix navar height style

--- src/app/layout/container/container.component.ts
+++ src/app/layout/container/container.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ContainerComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet.activatedRouteData.state;
--- src/app/pages/generic-crud/generic-crud.module.ts
+++ src/app/pages/generic-crud/generic-crud.module.ts
@@ -6,7 +6,6 @@ import { GenericWriteComponent } from './generic-write/generic-write.component';
 import { CrudManagerComponent } from './crud-manager/crud-manager.component';
 import { SemiTableModule } from '@widget/table/table.module';
 import { MaterialModule } from '@shared/common';
-import { ReactiveFormsModule } from '@angular/forms';
 import { GenericReadComponent } from './generic-read/generic-read.component';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { FormModule } from '@partials/form';
