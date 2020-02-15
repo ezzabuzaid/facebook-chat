c0f2adb0598d69f18e9ada1243ddc0df3723dcce
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-02-15 18:35:04 +0200

    remove firebase credintals

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   endpointUrl: 'https://node-buildozer.herokuapp.com/api',
-  serverOrigin: 'https://node-buildozer.herokuapp.com',
-  firebase: {
-    apiKey: 'AIzaSyBZtOQcwlZZYu7IEmuDnS2i_ziyVa2a5bo',
-    authDomain: 'chatbase-ebaa0.firebaseapp.com',
-    databaseURL: 'https://chatbase-ebaa0.firebaseio.com',
-    projectId: 'chatbase-ebaa0',
-    storageBucket: 'chatbase-ebaa0.appspot.com',
-    messagingSenderId: '483016343062'
-  }
+  serverOrigin: 'https://node-buildozer.herokuapp.com'
 };
