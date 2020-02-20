c6e97ebd9b2d4ef0703064854e85b021d4d47510
Ezz.Abuzaid <ezz.abuzaid@Equitis-MacBook-Pro.local>
2020-02-20 20:55:40 +0200

    remove ngx-perfect-scrollbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -13,7 +13,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NavbarItemComponent } from './components/navbar-item/navbar-item.component';
 import { NavbarCollapseComponent } from './components/navbar-collapse/navbar-collapse.component';
 import { NavbarHeaderComponent } from './components/navbar-header/navbar-header.component';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { DirectivesModule } from '@shared/directives';
 import { AppShellLoaderModule } from '@widget/app-shell-loader';
@@ -29,7 +28,6 @@ import { ChatCardModule } from '@partials/chat-card';
     MaterialModule,
     SidebarModule,
     TranslateModule.forChild(),
-    PerfectScrollbarModule,
     ScrollingModule,
     DirectivesModule,
     AppShellLoaderModule,
