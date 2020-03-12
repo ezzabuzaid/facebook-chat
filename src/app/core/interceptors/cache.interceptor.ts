b78b3a42a3c92e91d6a38d4c82026ec526fd34d0
Ezz.Abuzaid <ezz.abuzaid@Equitis-MacBook-Pro.local>
2020-03-12 18:51:24 +0200

    readjust cache interceptor

--- src/app/core/interceptors/cache.interceptor.ts
+++ src/app/core/interceptors/cache.interceptor.ts
@@ -1,40 +1,42 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable, } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
-import { tap, map, catchError } from 'rxjs/operators';
+import { Observable, from, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 import { HttpMethod, getHeader, ECustomHeaders } from '../http';
 import { Logger } from '@core/helpers/logger';
-import { CacheService } from '@core/helpers/cache';
+import { AppUtils } from '@core/helpers/utils';
+import { HttpCacheHelper } from '@core/helpers/cache';
 const log = new Logger('CacheInterceptor');
 @Injectable()
 export class CacheInterceptor implements HttpInterceptor {
     constructor(
-        private cacheService: CacheService
+        private cacheService: HttpCacheHelper
     ) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.cacheService.populate(getHeader<string>(req.headers, ECustomHeaders.CACHE_CATEGORY));
         const isCacheable = getHeader(req.headers, ECustomHeaders.LOCAL_CACHE);
-        if (req.method === HttpMethod.GET || !isCacheable) {
+        if (req.method !== HttpMethod.GET && !isCacheable) {
             return next.handle(req.clone());
+        } else {
+            this.cacheService.populate(getHeader<string>(req.headers, ECustomHeaders.CACHE_CATEGORY));
         }
-        const cachedValue = this.cacheService.get(req.urlWithParams);
-        return from(cachedValue)
+
+        return from(this.cacheService.get(req.urlWithParams))
             .pipe(
-                map(entry => {
-                    log.debug(`${req.method} Request for ${req.urlWithParams} fetched from cache`);
-                    return new HttpResponse(entry.response);
-                }),
-                catchError((error) => {
-                    return next.handle(req.clone())
-                        .pipe(
-                            tap((event: HttpResponse<any>) => {
+                switchMap(response => {
+                    if (AppUtils.isNullorUndefined(response)) {
+                        return next.handle(req.clone())
+                            .pipe(map((event: HttpResponse<any>) => {
                                 if (event instanceof HttpResponse) {
                                     this.cacheService.set(req.urlWithParams, event.clone());
                                 }
-                            })
-                        );
-                }),
+                                return event;
+                            }));
+                    } else {
+                        log.debug(`${req.method} Request for ${req.urlWithParams} fetched from cache`);
+                        return of(response);
+                    }
+                })
             );
     }
 }
