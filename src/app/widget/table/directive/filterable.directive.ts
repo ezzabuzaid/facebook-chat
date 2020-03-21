081d9b747118e1e96a5b3e796a004585b357ee8d
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-21 15:51:01 +0200

    add supported types to table

--- src/app/widget/table/directive/filterable.directive.ts
+++ src/app/widget/table/directive/filterable.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, OnInit, Input, Inject } from '@angular/core';
 import { TableComponent } from '../table-view/table-view.component';
+import { FilterTypes } from '../index';
 
 @Directive({
   selector: 'semi-table table thead tr:first-child semi-table-head > th'
@@ -8,7 +9,7 @@ import { TableComponent } from '../table-view/table-view.component';
 export class FilterableDirective implements OnInit {
   @Input() filterable: string = null;
   @Input() list: any[] = [];
-  @Input() type: string = 'text';
+  @Input() type: FilterTypes = 'text';
 
   constructor(
     @Inject(TableComponent) private tableComponent: TableComponent,
