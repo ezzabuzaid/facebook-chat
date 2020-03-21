52a1e0d2e962e6960fc116e25e347c5103465b5a
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-22 01:01:04 +0200

    use new conversation card with base chat card

--- src/app/shared/services/users/users.service.ts
+++ src/app/shared/services/users/users.service.ts
@@ -20,7 +20,7 @@ export class UsersService {
     }
 
     public searchForUsers(name: string) {
-        return this.http.get<UsersModel.IUser[]>(`${Constants.API.users.search}/${name}`);
+        return this.http.get<UsersModel.IUser[]>(`${Constants.API.users.search}?username=${name}`);
     }
 
     public getUsersWithoutMe() {
