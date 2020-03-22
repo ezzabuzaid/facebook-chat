ff34417ef365ce0aa58ba447b17667ebf88d600b
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-22 17:32:00 +0200

    implement create conversation and jumo to existing conversation

--- src/app/pages/chat/chat-create-card/chat-create-card.component.ts
+++ src/app/pages/chat/chat-create-card/chat-create-card.component.ts
@@ -6,10 +6,13 @@ import { UsersModel, ChatModel } from '@shared/models';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { FormControl } from '@angular/forms';
-import { map, switchMap, filter } from 'rxjs/operators';
+import { map, filter, share } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AppUtils, typeaheadOperator } from '@core/helpers/utils';
 import { ChatService } from '@shared/services/chat';
+import { UserCardComponent } from '../conversation-chat-card/conversation-chat-card.component';
+import { ChatManager } from '../chat.manager';
+import { TokenService } from '@core/helpers/token';
 
 @Component({
   selector: 'app-chat-create-card',
@@ -23,20 +26,17 @@ export class ChatCreateCardComponent implements OnInit, IChatCard<any> {
   selectedUsers: UsersModel.IUser[] = [];
   separatorKeysCodes: number[] = [ENTER, COMMA];
   autocompleteControl = new FormControl();
-  $conversation: Observable<{
-    conversation,
-    user
-  }> = null;
+  $conversation: Observable<ChatModel.IConversation> = null;
 
   constructor(
     private chatCardManager: ChatCardManager,
     private usersService: UsersService,
-    private chatService: ChatService
+    private chatService: ChatService,
   ) { }
 
   ngOnInit() {
     this.$users = this.autocompleteControl.valueChanges.pipe(
-      filter(AppUtils.isTruthy),
+      filter(value => AppUtils.isFalsy(AppUtils.isEmptyString(value))),
       typeaheadOperator((value) => this.usersService.searchForUsers(value)),
       map(users => users.filter(user => {
         return this.selectedUsers.findIndex(selectedUser => selectedUser._id === user._id) === -1
@@ -58,25 +58,38 @@ export class ChatCreateCardComponent implements OnInit, IChatCard<any> {
 
   removeUser(index: number) {
     this.selectedUsers.splice(index, 1);
-    this.tryConversation(this.selectedUsers[0]);
+    this.tryConversation(this.firstSelectedUser);
   }
 
   tryConversation(user: UsersModel.IUser) {
     if (this.selectedUsers.length === 1) {
       this.$conversation = this.chatService.getConversation(user._id)
-        .pipe(map((conversation) => {
-          if (conversation) {
-            return {
-              conversation,
-              user
-            }
-          }
-          return null;
-        }));
+        .pipe(share());
     } else {
       this.$conversation = null;
     }
 
+  }
+
+  createConvesationAndSendMessage(text: string) {
+    this.chatService.createConversation(this.firstSelectedUser._id, text)
+      .subscribe((conversation) => {
+        this.chatCardManager.open(UserCardComponent, {
+          id: this.firstSelectedUser._id,
+          data: this.firstSelectedUser
+        });
+      });
+  }
+
+  jumpToConversationCard(text: string) {
+    this.chatCardManager.open(UserCardComponent, {
+      id: this.firstSelectedUser._id,
+      data: this.firstSelectedUser
+    });
+  }
+
+  get firstSelectedUser() {
+    return this.selectedUsers[0];
   }
 
 }
