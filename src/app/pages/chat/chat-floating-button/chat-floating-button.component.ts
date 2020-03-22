3bc9539316dff460ec2e9c52652a3d823556d0ea
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-22 17:31:13 +0200

    remove chatCardDate from floating button component

--- src/app/pages/chat/chat-floating-button/chat-floating-button.component.ts
+++ src/app/pages/chat/chat-floating-button/chat-floating-button.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { ChatCardManager } from '../chat-card.manager';
 import { IChatCard } from '../index';
 import { UserCardComponent } from '../conversation-chat-card/conversation-chat-card.component';
-import { ChatCardData } from '../types';
+import { UsersModel } from '@shared/models';
 
 @Component({
   selector: 'app-chat-floating-button',
   templateUrl: './chat-floating-button.component.html',
   styleUrls: ['./chat-floating-button.component.scss']
 })
-export class ChatFloatingButtonComponent implements OnInit, IChatCard<ChatCardData>{
+export class ChatFloatingButtonComponent implements OnInit, IChatCard<UsersModel.IUser>{
 
-  data: ChatCardData;
+  data: UsersModel.IUser;
   id: string;
 
   constructor(
