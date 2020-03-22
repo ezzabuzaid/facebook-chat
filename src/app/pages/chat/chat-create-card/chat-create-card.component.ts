7d86dccefaf42b1ac8a67bba3da5a6f3f4a4a2cd
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-22 14:27:09 +0200

    add auto complete to message card

--- src/app/pages/chat/chat-create-card/chat-create-card.component.ts
+++ src/app/pages/chat/chat-create-card/chat-create-card.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ChatCardManager } from '../chat-card.manager';
 import { IChatCard } from '..';
 import { UsersService } from '@shared/services/users';
-import { UsersModel } from '@shared/models';
+import { UsersModel, ChatModel } from '@shared/models';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { FormControl } from '@angular/forms';
 import { map, switchMap, filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AppUtils, typeaheadOperator } from '@core/helpers/utils';
+import { ChatService } from '@shared/services/chat';
 
 @Component({
   selector: 'app-chat-create-card',
@@ -21,16 +22,21 @@ export class ChatCreateCardComponent implements OnInit, IChatCard<any> {
   $users: Observable<UsersModel.IUser[]> = null;
   selectedUsers: UsersModel.IUser[] = [];
   separatorKeysCodes: number[] = [ENTER, COMMA];
-
   autocompleteControl = new FormControl();
+  $conversation: Observable<{
+    conversation,
+    user
+  }> = null;
 
   constructor(
     private chatCardManager: ChatCardManager,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private chatService: ChatService
   ) { }
 
   ngOnInit() {
     this.$users = this.autocompleteControl.valueChanges.pipe(
+      filter(AppUtils.isTruthy),
       typeaheadOperator((value) => this.usersService.searchForUsers(value)),
       map(users => users.filter(user => {
         return this.selectedUsers.findIndex(selectedUser => selectedUser._id === user._id) === -1
@@ -42,14 +48,35 @@ export class ChatCreateCardComponent implements OnInit, IChatCard<any> {
     this.chatCardManager.removeCard();
   }
 
-  addUser(event: MatAutocompleteSelectedEvent, autoCompleteInput: HTMLInputElement) {
-    this.selectedUsers.push(event.option.value);
+  selectUser(event: MatAutocompleteSelectedEvent, autoCompleteInput: HTMLInputElement) {
+    const user = event.option.value as UsersModel.IUser;
+    this.selectedUsers.push(user);
     this.autocompleteControl.setValue(null);
     autoCompleteInput.value = '';
+    this.tryConversation(user);
   }
 
-  removeUser(index) {
+  removeUser(index: number) {
     this.selectedUsers.splice(index, 1);
+    this.tryConversation(this.selectedUsers[0]);
+  }
+
+  tryConversation(user: UsersModel.IUser) {
+    if (this.selectedUsers.length === 1) {
+      this.$conversation = this.chatService.getConversation(user._id)
+        .pipe(map((conversation) => {
+          if (conversation) {
+            return {
+              conversation,
+              user
+            }
+          }
+          return null;
+        }));
+    } else {
+      this.$conversation = null;
+    }
+
   }
 
 }
