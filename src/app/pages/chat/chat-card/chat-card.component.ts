3fdede1dca6605b620580a210fb1bc56ab3fd30b
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-03-22 22:28:10 +0200

    add dynamic height to base chat card component

--- src/app/pages/chat/chat-card/chat-card.component.ts
+++ src/app/pages/chat/chat-card/chat-card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-card',
   templateUrl: './chat-card.component.html',
   styleUrls: ['./chat-card.component.scss']
 })
-export class ChatCardComponent implements OnInit {
+export class ChatCardComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  constructor(
+    private elementRef: ElementRef<HTMLElement>
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  ngAfterContentInit() {
+    const header = this.getElement('[card-header]');
+    const content = this.getElement('mat-card-content');
+    content.style.setProperty('height', `calc(100% - ${header.clientHeight}px)`)
+  }
+
+  private getElement(selector: string) {
+    const element = this.elementRef.nativeElement;
+    return element.querySelector(selector) as HTMLElement;
   }
 
 }
