129bb9296d609cc62125fbc5f0aa7f768e26b6b3
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-05-02 19:15:12 +0300

    used form container component in login page

--- src/app/pages/portal/login/login.component.ts
+++ src/app/pages/portal/login/login.component.ts
@@ -1,8 +1,7 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '@shared/user';
-import { FormUtils } from '@partials/form';
 import { Field, Form, EFieldType } from '@shared/common';
 import { Validators } from '@angular/forms';
 import { MatCheckbox } from '@angular/material/checkbox';
@@ -13,40 +12,39 @@ import { PortalModel } from '@shared/models';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  host: {
+    class: 'bg--primary'
+  }
 })
-export class LoginComponent extends FormUtils<PortalModel.ILoginRequest> implements OnInit {
+export class LoginComponent implements OnInit {
+  form = new Form<PortalModel.ILoginRequest>([
+    new Field('username', {
+      label: 'placeholder_username',
+      autocomplete: 'username',
+      validation: {
+        validators: Validators.required
+      }
+    }),
+    new Field('password', {
+      label: 'placeholder_passowrd',
+      type: EFieldType.PASSWORD,
+      autocomplete: 'current-password',
+      validation: {
+        validators: Validators.required
+      }
+    })
+  ]);
 
   constructor(
     private portalService: UserService,
     private tokenHelper: TokenService,
     private router: Router
-  ) {
-    super(
-      new Form([
-        new Field('username', {
-          label: 'placeholder_username',
-          autocomplete: 'username',
-          validation: {
-            validators: Validators.required
-          }
-        }),
-        new Field('password', {
-          label: 'placeholder_passowrd',
-          type: EFieldType.PASSWORD,
-          autocomplete: 'current-password',
-          validation: {
-            validators: Validators.required
-          }
-        }),
-      ])
-    );
-  }
+  ) { }
 
   ngOnInit() { }
 
-  login(rememberMeCheckBox: MatCheckbox) {
-    const { valid, value } = this.form;
+  login({ valid, value }, rememberMeCheckBox: MatCheckbox) {
     if (valid) {
       this.portalService.login(value)
         .subscribe(data => {
